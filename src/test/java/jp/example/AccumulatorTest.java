c03a35642d1e7b5312cbfc08c17c1da041ead8f0
Playstay <ys0701f@gmail.com>
2022-05-01 14:43:41 +0900

    仕様変更によりテスト実施時の処理も変更

--- src/test/java/jp/example/AccumulatorTest.java
+++ src/test/java/jp/example/AccumulatorTest.java
@@ -1,7 +1,9 @@
 package jp.example;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -12,31 +14,43 @@ public class AccumulatorTest {
     
     @Test
     public void 簡単な計算が可能なこと() {
+        Accumulator accumulator = new Accumulator();
         List<Token> exp = new ArrayList<>();
         exp.add(new Token("1"));
         exp.add(new Token("2"));
         exp.add(new Token("+"));
         
-        assertEquals(new BigDecimal(3), Accumulator.caluculate(exp));
+        assertEquals(new BigDecimal(3), accumulator.caluculate(exp));
     }
     
     @Test
     public void 減算の順序があっていること(){
+        Accumulator accumulator = new Accumulator();
         List<Token> exp = new ArrayList<>();
         exp.add(new Token("1"));
         exp.add(new Token("2"));
         exp.add(new Token("-"));
         
-        assertEquals(new BigDecimal(-1), Accumulator.caluculate(exp));
+        assertEquals(new BigDecimal(-1), accumulator.caluculate(exp));
     }
     
     @Test
     public void 除算の順序があっていること(){
+        Accumulator accumulator = new Accumulator();
         List<Token> exp = new ArrayList<>();
         exp.add(new Token("1"));
         exp.add(new Token("3"));
         exp.add(new Token("/"));
         
-        assertEquals(new BigDecimal(1).divide(new BigDecimal(3)), Accumulator.caluculate(exp));
+        assertEquals(new BigDecimal(1).divide(new BigDecimal(3),MathContext.DECIMAL128), accumulator.caluculate(exp));
+    }
+    
+    
+    @Test
+    public void 計算処理が連続する場合でも処理ができること() {
+        Accumulator accumulator = new Accumulator();
+        List<Token> exp = Arrays.stream("1 2 + 4 5 - *".split(" ")).map(x -> new Token(x)).toList();
+        
+        assertEquals(new BigDecimal("-3"), accumulator.caluculate(exp));
     }
 }
