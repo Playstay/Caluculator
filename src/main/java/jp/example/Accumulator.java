da7e05f0507ee32fa834c646d47d868696186a7c
Playstay <ys0701f@gmail.com>
2022-05-01 14:24:14 +0900

    除算の際にコンテキストを設定するように変更

--- src/main/java/jp/example/Accumulator.java
+++ src/main/java/jp/example/Accumulator.java
@@ -1,51 +1,85 @@
 package jp.example;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.List;
 
 public class Accumulator {
-    public static BigDecimal caluculate(List<Token> expression) {
+    private static final MathContext DEAFAULT_CONTEXT = MathContext.DECIMAL128;
+
+    private MathContext context;
+
+    public Accumulator() {
+        this(DEAFAULT_CONTEXT);
+    }
+
+    public Accumulator(MathContext context) {
+        if (context == null) {
+            throw new NullPointerException();
+        }
+
+        this.context = context;
+    }
+
+    public BigDecimal caluculate(List<Token> expression) {
         Deque<BigDecimal> stack = new ArrayDeque<>();
         for (Token token : expression) {
-            if(token.isNumber()){
+            if (token.isNumber()) {
                 stack.push(token.getValue());
                 continue;
             }
-            
+
             BigDecimal result = execute(stack, token);
             stack.push(result);
         }
-        
-        if(stack.size()!=1){
+
+        if (stack.size() != 1) {
             throw new RuntimeException();
         }
         return stack.pop();
     }
-    
-    private static BigDecimal execute(Deque<BigDecimal> stack, Token operand){
+
+    private BigDecimal execute(Deque<BigDecimal> stack, Token operand) {
         // TODO: 除数、被除数に当たる汎用的な単語がわからん...
         BigDecimal b = stack.pop();
         BigDecimal a = stack.pop();
         BigDecimal result = null;
-        
-        switch(operand.getType()){
+
+        switch (operand.getType()) {
             case ADD:
-            result = a.add(b);
-            break;
+                result = add(a, b);
+                break;
             case SUB:
-            result = a.subtract(b);
-            break;
+                result = subtract(a, b);
+                break;
             case MUL:
-            result = a.multiply(b);
-            break;
+                result = multiply(a, b);
+                break;
             case DIV:
-            result = a.divide(b);
-            break;
+                result = divide(a, b);
+                break;
             default:
                 throw new IllegalArgumentException();
         }
+
         return result;
+    }
+
+    private BigDecimal add(BigDecimal addend, BigDecimal augend) {
+        return addend.add(augend);
+    }
+
+    private BigDecimal subtract(BigDecimal minuend, BigDecimal subtrahend) {
+        return minuend.subtract(subtrahend);
+    }
+
+    private BigDecimal multiply(BigDecimal multiplier, BigDecimal multiplicand) {
+        return multiplier.multiply(multiplicand);
+    }
+
+    private BigDecimal divide(BigDecimal dividend, BigDecimal divisor) {
+        return dividend.divide(divisor, context);
     }
 }
